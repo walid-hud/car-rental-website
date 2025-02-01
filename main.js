ef4ebf013faecc8e3ac77ff4b61657c4457a958e
walid-hud <walidthehud@gmail.com>
2025-02-01 20:10:52 +0100

    ditched tailwind_css lol

--- main.js
+++ main.js
@@ -21,18 +21,22 @@ const posts = [
     title: 'Third Post',
     content: 'This is the content of the third post.'
 }]
-app.use(express.static(path.join(__dirname , 'public' , 'images')))
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+// app.get('/public/input.css' , (req , res)=>{
+//     console.log(req.url)
+//     res.setHeader('Content-Type' , 'text/css')
+//     res.sendFile(req.url)
+//     res.end()
+// })
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views',  'index.html'));
 });
 
-app.get('/posts/:id' , (req ,res)=>{
-    const result = posts.filter(post => post.id === Number(req.params.id) )
-    
-    result.length == 0 ? res.write(JSON.stringify({error:'nothing found'})) : res.write(JSON.stringify(result))  
-    res.end()
-})
+
 
 app.get('/search' , (req , res)=>{
     const limit = parseInt(req.query.limit)
